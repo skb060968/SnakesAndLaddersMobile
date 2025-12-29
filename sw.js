87b7a7ac35a8f6c3aaa2331bb2ac4d9d20d5b4db
Sunil <skb060968@gmail.com>
2025-12-29 12:05:46 +0530

    Stable layout

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - Update detection + prompt support 
 ============================== */
 
-const CACHE_NAME = "snl-3d-v18"; // ⬅️ bump on every deploy
+const CACHE_NAME = "snl-3d-v19"; // ⬅️ bump on every deploy
 
 const ASSETS = [
   "./",
